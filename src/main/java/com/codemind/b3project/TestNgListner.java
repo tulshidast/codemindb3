e02663232b64b1e748dd0868eec6e3b72a24e58d
tulshidast <42244862+tulshidast@users.noreply.github.com>
2022-11-05 07:52:11 +0530

    changes due to maven build failure

--- src/main/java/com/codemind/b3project/TestNgListner.java
+++ src/main/java/com/codemind/b3project/TestNgListner.java
@@ -6,48 +6,48 @@ import org.testng.Reporter;
 
 public class TestNgListner implements ITestListener {
 
-	public void onTestStart(ITestResult result) {
-		Reporter.log("Test case started " + result.getName());
-
-	}
-
-	public void onTestSuccess(ITestResult result) {
-		Reporter.log("Test case passed " + result.getName());
-
-	}
-
-	public void onTestFailure(ITestResult result) {
-		// if (result.getStatus() == result.FAILURE) {
-		// try {
-		// UtilityMethods.getScreenShot(result.getName(),driver);
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// }
-
-		Reporter.log("Test case failed " + result.getName());
-
-	}
-
-	public void onTestSkipped(ITestResult result) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void onStart(ITestResult result) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void onFinish(ITestResult result) {
-		// TODO Auto-generated method stub
-
-	}
+//	public void onTestStart(ITestResult result) {
+//		Reporter.log("Test case started " + result.getName());
+//
+//	}
+//
+//	public void onTestSuccess(ITestResult result) {
+//		Reporter.log("Test case passed " + result.getName());
+//
+//	}
+//
+//	public void onTestFailure(ITestResult result) {
+//		// if (result.getStatus() == result.FAILURE) {
+//		// try {
+//		// UtilityMethods.getScreenShot(result.getName(),driver);
+//		// } catch (IOException e) {
+//		// // TODO Auto-generated catch block
+//		// e.printStackTrace();
+//		// }
+//		// }
+//
+//		Reporter.log("Test case failed " + result.getName());
+//
+//	}
+//
+//	public void onTestSkipped(ITestResult result) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	public void onStart(ITestResult result) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	public void onFinish(ITestResult result) {
+//		// TODO Auto-generated method stub
+//
+//	}
 
 }
